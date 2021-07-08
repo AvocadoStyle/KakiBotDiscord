1593b5733ba0a7153be900d31bf188170f4fa4e6
AvocadoStyle <42403145+AvocadoStyle@users.noreply.github.com>
2021-07-08 14:20:48 +0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-import discord
-import DICT
-from bots import MyClient
-if __name__ == '__main__':
-    client = MyClient()
-    client.run(DICT.T["TOKEN"])
-
-
-#TOKEN: ODYyMzkwNDIwMDg3NzY3MDgw.YOXpqg.-DxFz3xREzdxuEm2AP1b8lmgbSY
